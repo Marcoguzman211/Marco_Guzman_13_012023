1dc9cf60cf4449409e0fa6dc78e0a53565b760df
Marcoguzman211 <guzmanescobar3@gmail.com>
2023-01-09 12:21:24 +0100

    Router and pages setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+/*import { BrowserRouter as Router, Routes, Route} from "react-router-dom";*/
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Error from "./pages/Error";
+import {home, login, profile} from "./routes";
+
+
+const router = createBrowserRouter([
+    {
+        path: home,
+        element: <Home />,
+    },
+    {
+        path: login,
+        element: <Login />,
+    },
+    {
+        path: profile,
+        element: <Profile />,
+    },
+    {
+        path: "/*",
+        element: <Error />,
+    }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <RouterProvider router={router} />
+   </>
   );
 }
 
